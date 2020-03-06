21f357d7fa7978aecf0c4e55155af15de26ebe6f
Ashish Kotnala <ashishkotnala1.ak@gmail.com>
2020-03-07 01:17:40 +0530

    implement OAuth2 github/facebook/google

--- src/main/java/com/ashish/springbootsecurity/SecurityConfiguration.java
+++ src/main/java/com/ashish/springbootsecurity/SecurityConfiguration.java
@@ -1,25 +1,40 @@
 package com.ashish.springbootsecurity;
 
-import org.springframework.context.annotation.Bean;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.crypto.password.NoOpPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.HttpStatusEntryPoint;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 
 @EnableWebSecurity
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
-	@Override
-	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-		auth.inMemoryAuthentication()
-		.withUser("foo")
-		.password("bar")
-		.roles("USER");
-	}
+	@Autowired
+	private CustomAuthenticationFailureHandler handler;
 	
-	@Bean
-	public PasswordEncoder getPasswordEncoder() {
-		return NoOpPasswordEncoder.getInstance();
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.authorizeRequests(a -> a
+				.antMatchers("/","/error","/webjars/**").permitAll()
+				.anyRequest().authenticated()
+				)
+				.exceptionHandling(e -> e
+						.authenticationEntryPoint(
+								new HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED))
+						)
+				.logout(l -> l
+						.logoutSuccessUrl("/").permitAll()
+						)
+				.csrf(c -> c
+						.csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
+						)
+				.oauth2Login(o -> o
+						.failureHandler((req, res, ex) -> {
+							req.getSession().setAttribute("error.message", ex.getMessage());
+							handler.onAuthenticationFailure(req, res, ex);
+						})
+		);
 	}
 }
